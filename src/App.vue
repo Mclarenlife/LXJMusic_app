87f437b5eca45e75ea762098a065b2faeb56de33
Mclarenlife <2457756671@qq.com>
2024-11-13 16:31:58 +0800

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <RouterView />
+    <div class="home_header">
+        <MyHeader></MyHeader>
+    </div>
+    <div>
+        <RouterView />
+    </div>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
-
+// import { RouterView } from 'vue-router';
+import MyHeader from '@/components/Header.vue';
 
 </script>
 
 <style scoped>
+.home_-header {
+    box-sizing: border-box;
+    padding: 10px;
+}
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="my_header">
+        <!-- 左侧 -->
+         <div class="header_left">
+            <van-icon name="wap-nav" size="22px" />
+         </div>
+        <!-- 中间 -->
+         <div class="header_content">
+            <div :class="{ active: currentTab === 1}" @click="setCurrentTab(1)">我的</div>
+            <div :class="{ active: currentTab === 2}" @click="setCurrentTab(2)">发现</div>
+            <div :class="{ active: currentTab === 3}" @click="setCurrentTab(3)">云村</div>
+            <div :class="{ active: currentTab === 4}" @click="setCurrentTab(4)">视频</div>
+         </div>
+        <!-- 右侧 -->
+         <div class="header_right">
+            <van-icon name="search" size="22px" />
+         </div>
+    </div>
+</template>
+
+<script setup>
+import  router from '@/router'
+import {ref} from 'vue'
+
+const currentTab = ref(2);
+
+const setCurrentTab = (tab) => {
+    currentTab.value = tab;
+    router.push(
+        { name: tab === 1 ? 'mine' : tab === 2 ? 'discovery' : tab === 3 ? 'home' : 'video' }
+    )
+};
+
+</script>
+
+<style scoped>
+.my_header {
+    display: flex;
+    justify-content: space-between;
+}
+
+.header_content {
+    display: flex;
+    justify-content: space-around;
+    width: 200px;
+}
+</style>
